a43d8cbcf9fdc9e0629a81a28770e280978ef253
qt <qtbytes@outlook.com>
2024-08-15 12:51:13 +0800

    update 3148: dp

---
+++ python/3148.maximum-difference-score-in-a-grid/solution.py
@@ -0,0 +1,41 @@
+# Created by none at 2024/08/15 12:40
+# leetgo: 1.4.7
+# https://leetcode.cn/problems/maximum-difference-score-in-a-grid/
+
+from typing import *
+from leetgo_py import *
+
+import bisect
+import collections
+import functools
+import heapq
+import itertools
+import operator
+import math
+import string
+
+# @lc code=begin
+
+
+class Solution:
+    def maxScore(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        res = -math.inf
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                x = grid[i][j]
+                if i + 1 < m:
+                    grid[i][j] = max(grid[i][j], grid[i + 1][j])
+                    res = max(res, grid[i + 1][j] - x)
+                if j + 1 < n:
+                    grid[i][j] = max(grid[i][j], grid[i][j + 1])
+                    res = max(res, grid[i][j + 1] - x)
+        return res
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    grid: List[List[int]] = deserialize("List[List[int]]", read_line())
+    ans = Solution().maxScore(grid)
+    print("\noutput:", serialize(ans, "integer"))
