7e9209a2030e20bd346a573efd1484e30748f1e1
qt <qtbytes@outlook.com>
2024-09-04 20:26:41 +0800

    update 2860: sort

--- python/2860.happy-students/solution.py
+++ python/2860.happy-students/solution.py
@@ -6,24 +6,26 @@ from typing import *
 from leetgo_py import *
 
 import bisect
-import collections 
+import collections
 import functools
-import heapq 
-import itertools 
+import heapq
+import itertools
 import operator
-import math 
+import math
 import string
 
 # @lc code=begin
+
 
 class Solution:
     def countWays(self, nums: List[int]) -> int:
         nums.sort()
         res = 0
         n = len(nums)
-        for i in range(n-1, -1,-1):
-            res += (nums[i] < i + 1) and  (i + 1 >= n or i + 1 < nums[i+1])
+        for i in range(n - 1, -1, -1):
+            res += (nums[i] < i + 1) and (i + 1 >= n or i + 1 < nums[i + 1])
         return res + (nums[0] > 0)
+
 
 # @lc code=end
 
