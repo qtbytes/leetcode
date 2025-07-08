fd5be9b59441d2e44b6c85b89cf56aef6b812b02
qt <qtbytes@outlook.com>
2025-07-08 15:08:49 +0800

    update 1751: dp + binary search

--- rust/src/1751.maximum-number-of-events-that-can-be-attended-ii/solution.rs
+++ rust/src/1751.maximum-number-of-events-that-can-be-attended-ii/solution.rs
@@ -22,32 +22,16 @@ impl Solution {
         events.sort_unstable_by_key(|e| e[1]);
 
         // dp[i][j] = e[i][2] + max(dp[x][j-1]) if e[x][1] < e[i][0]
-        let mut prev = vec![];
-        prev.push((0, 0));
-        let mut dp = vec![vec![-1; k + 1]; n + 1];
-        let mut res = 0;
-        for j in 1..=k {
-            let mut st = vec![];
-            st.push((0, 0));
-            for i in 0..n {
-                let e = &events[i];
-                let x = prev
-                    .binary_search_by_key(&(e[0]), |&(_, r)| r)
-                    .unwrap_or_else(|x| x)
-                    - 1;
-                dp[i + 1][j] = (prev[x].0 + e[2]).max(dp[i][j]);
-                res = res.max(dp[i + 1][j]);
-                while e[1] <= st.last().unwrap().1 {
-                    st.pop();
-                }
-                st.push((dp[i + 1][j], e[1]));
-                // assert!(dp[i + 1][j] >= dp[i + 1][j - 1])
+        let mut dp = vec![vec![0; k + 1]; n + 1];
+        for (i, e) in events.iter().enumerate() {
+            for j in 1..=k {
+                let x = events[..i].partition_point(|event| event[1] < e[0]);
+                dp[i + 1][j] = (dp[x][j - 1] + e[2]).max(dp[i][j]);
             }
-            prev = st;
         }
         // println!("{events:?}");
         // println!("{dp:?}");
-        res
+        dp[n][k]
     }
 }
 
