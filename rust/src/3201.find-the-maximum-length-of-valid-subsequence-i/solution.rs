204afc0fb39997f4cf78480279403de60cdfb93f
qt <qtbytes@outlook.com>
2025-07-16 11:08:29 +0800

    update 3201: dp (better impl)

--- rust/src/3201.find-the-maximum-length-of-valid-subsequence-i/solution.rs
+++ rust/src/3201.find-the-maximum-length-of-valid-subsequence-i/solution.rs
@@ -16,37 +16,23 @@ use std::mem::swap;
 
 impl Solution {
     pub fn maximum_length(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        let b: Vec<i32> = nums.into_iter().map(|x| x & 1).collect();
-        let mut f = vec![vec![0; 3]; n];
-        // (0,1) | (1,0)
-        // 1,1
-        // 0,0
-        let target = vec![1, 0, 0];
-        let mut last: Vec<Option<usize>> = vec![None; 3];
-
-        let mut res = 0;
-
-        for (i, x) in b.iter().enumerate() {
-            for j in 0..3 {
-                if let Some(k) = last[j] {
-                    if x ^ b[k] == target[j] {
-                        f[i][j] = f[k][j] + 1;
-                        last[j] = Some(i);
-                    }
-                } else {
-                    if j == 0 || j & 1 == *x as usize {
-                        last[j] = Some(i);
-                        f[i][j] = 1
-                    }
+        fn find(nums: &Vec<i32>, last: i32, target: i32) -> i32 {
+            let mut res = 0;
+            let mut last = last;
+            for &x in nums {
+                let x = x & 1;
+                if x ^ last == target {
+                    res += 1;
+                    last = x;
                 }
-                res = res.max(f[i][j])
             }
+            res
         }
 
-        // println!("{:?}", f);
-
-        res
+        find(&nums, 0, 1)
+            .max(find(&nums, 1, 1))
+            .max(find(&nums, 0, 0))
+            .max(find(&nums, 1, 0))
     }
 }
 
