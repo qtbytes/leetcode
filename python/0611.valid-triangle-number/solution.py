d8c03a7a350eeec4a443f3434aec1934c981a330
qt <qtbytes@outlook.com>
2025-09-26 13:04:32 +0800

    style: better way to ignore zero

--- python/0611.valid-triangle-number/solution.py
+++ python/0611.valid-triangle-number/solution.py
@@ -22,15 +22,11 @@ class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums.sort()
         n = len(nums)
-        # remove zero
-        i = 0
-        while i < n and nums[i] == 0:
-            i += 1
-        nums = nums[i:]
-        n = len(nums)
 
         res = 0
         for i in range(n - 2):
+            if nums[i] == 0:
+                continue
             k = i + 2
             for j in range(i + 1, n - 1):
                 # k = bisect_left(nums, nums[i] + nums[j])
