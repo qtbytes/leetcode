1daba888860bd11d2e22e05f4133d6bb72a9be7f
qt <qtbytes@outlook.com>
2025-10-06 13:57:06 +0800

    update 0778: shortest path

--- rust/src/0778.swim-in-rising-water/solution.rs
+++ rust/src/0778.swim-in-rising-water/solution.rs
@@ -15,49 +15,36 @@ use std::collections::*;
 use std::mem::swap;
 
 impl Solution {
-    pub fn swim_in_water(grid: Vec<Vec<i32>>) -> i32 {
+    pub fn swim_in_water(mut grid: Vec<Vec<i32>>) -> i32 {
         let n = grid.len();
-        let mut fa: Vec<usize> = (0..n * n).collect();
-
-        fn find(x: usize, fa: &mut Vec<usize>) -> usize {
-            if fa[x] != x {
-                fa[x] = find(fa[x], fa)
-            }
-            fa[x]
-        }
-
-        fn union(x: usize, y: usize, fa: &mut Vec<usize>) -> bool {
-            let (fx, fy) = (find(x, fa), find(y, fa));
-            if fx == fy {
-                return false;
-            }
-            fa[fy] = fx;
-            true
-        }
-
         let mut q = BinaryHeap::new();
-
-        for (i, row) in grid.iter().enumerate() {
-            for (j, &t) in row.iter().enumerate() {
-                q.push((-t, i, j));
+        fn add(
+            q: &mut BinaryHeap<(i32, usize, usize)>,
+            x: usize,
+            y: usize,
+            grid: &mut Vec<Vec<i32>>,
+        ) {
+            if grid[x][y] == -1 {
+                return;
             }
+            q.push((-grid[x][y], x, y));
+            grid[x][y] = -1;
         }
-
-        let dirs = [0, -1, 0, 1, 0];
+        add(&mut q, 0, 0, &mut grid);
+        let dirs = [0, 1, 0, -1, 0];
+        let mut res = 0;
         while let Some((t, i, j)) = q.pop() {
+            res = res.max(-t);
+            if (i, j) == (n - 1, n - 1) {
+                return res;
+            }
             for d in 0..4 {
                 let (x, y) = (i as i32 + dirs[d], j as i32 + dirs[d + 1]);
-                if x >= 0 && x < n as i32 && y >= 0 && y < n as i32 {
-                    if grid[x as usize][y as usize] < -t {
-                        union(i * n + j, x as usize * n + y as usize, &mut fa);
-                    }
+                if 0 <= x && x < n as i32 && 0 <= y && y < n as i32 {
+                    add(&mut q, x as usize, y as usize, &mut grid);
                 }
             }
-            if find(0, &mut fa) == find(n * n - 1, &mut fa) {
-                return -t;
-            }
         }
-
         unreachable!()
     }
 }
