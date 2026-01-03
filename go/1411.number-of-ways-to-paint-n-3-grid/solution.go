c724670af942bf466ed3867cb31266f16688ac12
qt <qtbytes@outlook.com>
2026-01-03 16:23:10 +0800

    update 1411: dp

---
+++ go/1411.number-of-ways-to-paint-n-3-grid/solution.go
@@ -0,0 +1,81 @@
+// Created by none at 2026/01/03 16:00
+// leetgo: dev
+// https://leetcode.com/problems/number-of-ways-to-paint-n-3-grid/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+const MOD int = 1e9 + 7
+
+func numOfWays(n int) int {
+	points := [][3]int{}
+	for i := range 3 {
+		for j := range 3 {
+			if i == j {
+				continue
+			}
+			for k := range 3 {
+				if j == k {
+					continue
+				}
+				p := [3]int{i, j, k}
+				points = append(points, p)
+			}
+		}
+	}
+
+	g := make([][]int, len(points))
+	for i, x := range points {
+		for j, y := range points {
+			ok := func(x, y [3]int) bool {
+				for i := range x {
+					if x[i] == y[i] {
+						return false
+					}
+				}
+				return true
+			}
+			if x != y && ok(x, y) {
+				g[i] = append(g[i], j)
+			}
+		}
+	}
+
+	prev := make([]int, len(points))
+	for i := range prev {
+		prev[i] = 1
+	}
+	for range n - 1 {
+		next := make([]int, len(points))
+		for x, c := range prev {
+			for y := range g[x] {
+				next[y] = (next[y] + c) % MOD
+			}
+		}
+		prev = next
+	}
+
+	ans := 0
+	for _, x := range prev {
+		ans = (ans + x) % MOD
+	}
+	return ans
+}
+
+// @lc code=end
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	n := Deserialize[int](ReadLine(stdin))
+	ans := numOfWays(n)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
