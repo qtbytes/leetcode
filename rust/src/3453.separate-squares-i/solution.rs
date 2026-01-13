7fb928ecc1a790018da7dc334f126ce5870ab5fc
qt <qtbytes@outlook.com>
2026-01-13 19:50:15 +0800

    update 3453: diff array

--- rust/src/3453.separate-squares-i/solution.rs
+++ rust/src/3453.separate-squares-i/solution.rs
@@ -16,32 +16,36 @@ use std::mem::swap;
 
 impl Solution {
     pub fn separate_squares(squares: Vec<Vec<i32>>) -> f64 {
-        let mut l: f64 = 0.0;
-        let mut r: f64 = 1e9;
+        let mut diff = HashMap::new();
+        let mut total: i64 = 0;
 
-        while r - l > 1e-6 {
-            let mid = (l + r) / 2.0;
-            let check = |mid: f64| -> bool {
-                let mut ans = 0.0;
-                for s in &squares {
-                    let y = s[1] as f64;
-                    let l = s[2] as f64;
-                    let area = l * l;
-                    if y + l <= mid {
-                        ans -= area
-                    } else if y >= mid {
-                        ans += area
-                    } else {
-                        //(x+l-mid)/l - (mid-x)/l = (x*2+l-mid*2)/l
-                        ans += area * (y * 2.0 + l - mid * 2.0) / l
-                    }
-                }
-                ans <= 1e-5
-            };
-            if check(mid) { r = mid } else { l = mid + 1e-9 }
+        for s in &squares {
+            let y = s[1];
+            let l = s[2];
+            *diff.entry(y).or_insert(0) += l;
+            *diff.entry(y + l).or_insert(0) -= l;
+            total += (l as i64) * (l as i64)
         }
 
-        r
+        let mut keys: Vec<i32> = diff.keys().cloned().collect();
+        keys.sort_unstable();
+
+        let mut sum_x = 0;
+        let mut area = 0;
+
+        for w in keys.windows(2) {
+            let y1 = w[0];
+            let y2 = w[1];
+            sum_x += diff[&y1];
+            area += sum_x as i64 * (y2 - y1) as i64;
+            if area * 2 >= total {
+                // (area - (y2-y) * sum_x) * 2 == total
+                // (total / 2 - area) / sum_x + y2
+                return y2 as f64 + (total as f64 / 2.0 - area as f64) / sum_x as f64;
+            }
+        }
+
+        unreachable!()
     }
 }
 
