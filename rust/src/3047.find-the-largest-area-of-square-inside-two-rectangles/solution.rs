93af81cf0f77a7d629da52a78195a62ded1aded9
qt <qtbytes@outlook.com>
2026-01-17 14:18:23 +0800

    update 3047: optimized enumerate

--- rust/src/3047.find-the-largest-area-of-square-inside-two-rectangles/solution.rs
+++ rust/src/3047.find-the-largest-area-of-square-inside-two-rectangles/solution.rs
@@ -18,9 +18,9 @@ impl Solution {
     pub fn largest_square_area(bottom_left: Vec<Vec<i32>>, top_right: Vec<Vec<i32>>) -> i64 {
         let n = bottom_left.len();
 
-        let mut ans = 0;
+        let mut max_l = 0;
 
-        let calc = |i: usize, j: usize| -> i64 {
+        let calc = |i: usize, j: usize| -> i32 {
             let x1 = max(bottom_left[i][0], bottom_left[j][0]);
             let y1 = max(bottom_left[i][1], bottom_left[j][1]);
             let x2 = min(top_right[i][0], top_right[j][0]);
@@ -29,19 +29,22 @@ impl Solution {
             if x1 >= x2 || y1 >= y2 {
                 0
             } else {
-                let l = (x2 - x1).min(y2 - y1) as i64;
-                l * l
+                (x2 - x1).min(y2 - y1)
             }
         };
 
         for i in 0..n {
+            let l = top_right[i][0] - bottom_left[i][0];
+            if l <= max_l {
+                continue;
+            }
             for j in 0..i {
                 // check the intersecting region of rec[i] and rec[j]
-                ans = max(ans, calc(i, j))
+                max_l = max(max_l, calc(i, j))
             }
         }
 
-        ans
+        max_l as i64 * max_l as i64
     }
 }
 
